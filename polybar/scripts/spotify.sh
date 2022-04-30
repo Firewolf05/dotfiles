bc29abc7656397500358af0833bc156dfa5be066
George <77410335+Firewolf05@users.noreply.github.com>
2022-04-30 18:12:24 +0300

    Add files via upload

--- polybar/scripts/spotify.sh
+++ polybar/scripts/spotify.sh
@@ -1,4 +1,4 @@
 if [ $(playerctl metadata -f '{{playerName}}') == 'spotify' ]
 then
-    echo $(playerctl metadata -f '{{artist}} - {{title}}' | sed -E 's/(.{30}).+/\1.../') 
+    echo  $(playerctl metadata -f '{{artist}} - {{title}}')
 fi
