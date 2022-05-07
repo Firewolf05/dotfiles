e2f55e070a9b088b4aef057b55291f8e13193fe7
George <77410335+Firewolf05@users.noreply.github.com>
2022-05-07 17:55:20 +0300

    config update v2

---
+++ rofi/scripts/powermenu.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+style="rounded"
+
+dir="$HOME/.config/rofi/"
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+uptime=$(uptime -p | sed -e 's/up //g')
+cpu=$($HOME/.config/rofi/scripts/usedcpu)
+memory=$($HOME/.config/rofi/scripts/usedram)
+
+# Options
+shutdown=""
+reboot=""
+lock=""
+suspend=""
+logout=""
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $HOME/.config/rofi-new/confirm.rasi
+}
+
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "祥  $uptime  |     $cpu  |  ﬙  $memory " -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl poweroff
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			systemctl poweroff 
+        fi
+        ;;
+    $reboot)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl reboot
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			systemctl reboot
+        fi
+        ;;
+    $lock)
+		if [[ -f /usr/bin/i3lock ]]; then
+			i3lock
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $suspend)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+        fi
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			fi
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			bspc quit
+        fi
+        ;;
+esac
