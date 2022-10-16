74e0ceadd73099028605847da2351b75972e36ac
George <77410335+Firewolf05@users.noreply.github.com>
2022-10-16 21:36:59 +0300

    Delete polybar directory

--- polybar/scripts/calendar.sh
+++
@@ -1,6 +0,0 @@
-if [ $(bspc query -M -m --names) == "HDMI-A-0" ];
-then 
-    $HOME/.config/eww/bar/scripts/popup calendar
-else 
-    $HOME/.config/eww/bar2/scripts/popup calendar
-fi 
--- polybar/scripts/spotify.sh
+++
@@ -1,4 +0,0 @@
-#if [ $(playerctl metadata -f '{{playerName}}') == 'spotify' ]
-if pgrep spotify >/dev/null ; then
-    echo $(playerctl --player=spotify metadata -f '{{artist}} - {{title}}')
-fi
